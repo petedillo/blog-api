4a8fd3230cfc6549f38500e5c8fcdc234d144e76
pedro <pedelgadillo@gmail.com>
2025-11-01 19:03:29 -0500

    init

---
+++ src/test/java/com/petedillo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petedillo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
