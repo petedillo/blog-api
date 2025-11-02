2bfe54386a2a449606d8685a3db89afb4260c87d
pedro <pedelgadillo@gmail.com>
2025-11-01 21:01:09 -0500

    feat: extend ApiController with blog post endpoints for fetching and search

--- src/main/java/com/petedillo/api/controller/ApiController.java
+++ src/main/java/com/petedillo/api/controller/ApiController.java
@@ -1,20 +1,40 @@
 package com.petedillo.api.controller;
 
 
+import com.petedillo.api.model.BlogPost;
+import com.petedillo.api.service.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1")
 public class ApiController {
 
-    @GetMapping("health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+    private final BlogPostService blogPostService;
+
+    @Autowired
+    public ApiController(BlogPostService blogPostService) {
+        this.blogPostService = blogPostService;
     }
 
+    @GetMapping("health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Health Check");
+    }
 
+    @GetMapping("posts")
+    public ResponseEntity<List<BlogPost>> getPosts() {
+        return ResponseEntity.ok(blogPostService.getAllPosts());
+    }
+
+    @GetMapping("search")
+    public ResponseEntity<String> searchPosts() {
+        return ResponseEntity.ok("Search");
+    }
 
 }
